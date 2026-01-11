e1c090d8f2f221ec22dbd2a43d5853a146d6b165
Maksym-Shekhovtsov <maksmsh@edu.aau.at>
2026-01-11 18:41:31 +0100

    Fixed passwords not being hashed during database creation.

--- backend/initdb/02_users_data.sql
+++ backend/initdb/02_users_data.sql
@@ -2,7 +2,12 @@ INSERT INTO u_status (name, description) VALUES
      ('Active', 'Standard'),
      ('Suspended', 'User violated terms of service');
 
+
+--Passwords must be prehashed - 10 rounds of salting with Bcrypt
 INSERT INTO users(email, password, username, user_type, status_id) VALUES
-	('admin', 'admin', 'admin', 'Admin', 1),
-	('george@mail.com', 'pwd', 'george', 'Customer', '1'),
-	('lisa@mail.com', 'pwd', 'lisa', 'Customer', '1');
+--Admin Pass: admin
+	('admin', '$2a$12$zTRYyYVrTmIMhdgkS2LY7.5TXiR0yb95bb9QVvJ4Y3UHZJRQTVPRW', 'admin', 'Admin', 1),
+--George Pass: pwd
+--Lisa Pass: pwd
+	('george@mail.com', '$2a$12$XFSm1i.8b0URsvtBs1JqSehmFo1w1TUS.yHmZTEtdRqG2gHoyLzby', 'george', 'Customer', '1'),
+	('lisa@mail.com', '$2a$12$XFSm1i.8b0URsvtBs1JqSehmFo1w1TUS.yHmZTEtdRqG2gHoyLzby', 'lisa', 'Customer', '1');
