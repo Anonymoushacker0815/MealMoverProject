131dc74d07406842cfe295ece69030ed94b44b9c
SoupCube <66270931+SoupCube@users.noreply.github.com>
2026-01-19 10:00:36 +0100

    added insert statement for ordered dishes

--- backend/initdb/08_orders_data.sql
+++ backend/initdb/08_orders_data.sql
@@ -9,3 +9,10 @@ INSERT INTO orders (customer_id, restaurant_id, status_id, price) VALUES
 (2, 3, 2, 23.50),
 (3, 2, 1, 39.99),
 (2, 1, 4, 20.00);
+
+INSERT INTO o_dishes (order_id, dish_id, ammount) VALUES
+(1,2,5),
+(1,1,2),
+(2,3,2),
+(2,4,5),
+(3,3,1);
