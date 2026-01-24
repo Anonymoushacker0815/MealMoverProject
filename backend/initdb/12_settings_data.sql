ba53ad32eedc8983ddf013a8d63a3ac3a5c09804
SoupCube <66270931+SoupCube@users.noreply.github.com>
2026-01-24 13:09:22 +0100

    added information comment

--- backend/initdb/12_settings_data.sql
+++ backend/initdb/12_settings_data.sql
@@ -1,8 +1,9 @@
 INSERT INTO settings (delivery_distance, discount, service_fee) VALUES
 (10, 3, 13.50);
-
+--percent for percent discounts like 5%
+--and amount for fixed amounts like 10€
 INSERT INTO discount_codes (code, percent, amount) VALUES
 ('disc', 5, null),
-('dis43', null, 4.3),
+('dis43', null, 5.00),
 ('less50', 50, null),
-('reduce13', null, 13.00);
+('reduce10', null, 10.00);
