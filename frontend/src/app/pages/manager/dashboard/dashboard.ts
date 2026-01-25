68345242b1dc700a5356463160baf8ccdc545e37
SoupCube <66270931+SoupCube@users.noreply.github.com>
2026-01-25 18:19:25 +0100

    dashboard types added

--- frontend/src/app/pages/manager/dashboard/dashboard.ts
+++ frontend/src/app/pages/manager/dashboard/dashboard.ts
@@ -1,6 +1,39 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Navbar } from '../../../components/navbar/navbar';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+
+type Income = {
+  amount: number;
+}
+
+type UserCount = {
+  status: string;
+  count: number;
+}
+
+type PendingRegs = {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+type OrderStats = {
+  id: number;
+  count: number;
+  sum: number;
+}
+
+type Restaurant = {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  opening_hours: string;
+  status: string;
+}
 
 @Component({
   standalone: true,
